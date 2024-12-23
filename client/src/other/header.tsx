7039b65c958cd6af7d5137a0a46af5a134692deb
singsebbo <sebbolam@gmail.com>
2024-12-22 19:08:33 -0800

    Fix minor positioning error of header elements

--- client/src/other/header.tsx
+++ client/src/other/header.tsx
@@ -3,15 +3,15 @@ import ProfileSVG from "../assets/profile-svg";
 function Header() {
   return (
     <>
-      <div id="header" className="flex align-center mt-4 mx-4">
+      <div id="header" className="flex align-center justify-center mt-4 mx-4">
         <a 
-          className="transition hover:scale-110 duration-150 text-red-500 text-4xl h-10 mx-auto"
+          className="transition hover:scale-110 duration-150 text-red-500 text-4xl h-10"
           href="https://github.com/timmyvo54/KaraokeTube"
           target="_blank"
         >
           KaraokeTube
         </a>
-        <div className="flex self-center transition hover:scale-125 duration-150">
+        <div className="flex self-center transition hover:scale-125 duration-150 absolute right-4">
           <ProfileSVG/>
         </div>
       </div>
