8fc2d0a8bbee70c5e2c8c7a945d0b45768b80756
singsebbo <sebbolam@gmail.com>
2024-12-22 19:01:39 -0800

    Change code styling of HTML tag

--- client/src/other/footer.tsx
+++ client/src/other/footer.tsx
@@ -4,7 +4,12 @@ function Footer() {
   return (
     <>
       <div id="footer" className="flex justify-center mb-4">
-        <a id="github" className="transition hover:scale-125 duration-150" href="https://github.com/timmyvo54/KaraokeTube" target="_blank">
+        <a
+          id="github"
+          className="transition hover:scale-125 duration-150"
+          href="https://github.com/timmyvo54/KaraokeTube"
+          target="_blank"
+        >
           <GitHubSVG/>
         </a>
       </div>
