443001b35f0da1bfec1db5f4d886b694be0d19c7
singsebbo <sebbolam@gmail.com>
2025-01-04 17:15:13 -0800

    Setup server for CORS on development environment

--- server/src/config.ts
+++ server/src/config.ts
@@ -6,3 +6,8 @@ export const DATABASE_URL: string = process.env.DATABASE_URL as string;
 if (!DATABASE_URL) {
   throw new Error("Missing MongoDB Connection String!");
 }
+
+export const NODE_ENV: string = process.env.NODE_ENV as string;
+if (!NODE_ENV) {
+  throw new Error("Missing NODE_ENV string!");
+}
